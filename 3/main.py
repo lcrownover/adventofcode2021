92efd925df1825761366bed9f7c6576282c4451f
Lucas Crownover <lcrownover127@gmail.com>
2021-12-03 16:37:30 -0800

    day 3 done yay

--- 3/main.py
+++ 3/main.py
@@ -23,45 +23,27 @@ def first(inputs):
 def second(inputs):
     ogr = [i for i in inputs]
     csr = [i for i in inputs]
+    for c in range(len(inputs[0])):
+        if len(ogr) == 1:
+            break
+        x = sum([1 for line in ogr if line[c] == "1"])
+        if x >= (len(ogr) / 2):
+            ogr = [i for i in ogr if list(i)[c] == "1"]
+        else:
+            ogr = [i for i in ogr if list(i)[c] == "0"]
 
     for c in range(len(inputs[0])):
-        print("ogr")
-        x = sum([1 for line in inputs if line[c] == "1"])
-        if x >= ( len(inputs) / 2 ):
-            print(f"place {c+1} == 1")
-            if len(ogr) > 1:
-                ogr = [i for i in ogr if list(i)[c] == "1"]
+        if len(csr) == 1:
+            break
+        x = sum([1 for line in csr if line[c] == "1"])
+        if x >= (len(csr) / 2):
+            csr = [i for i in csr if list(i)[c] == "0"]
         else:
-            print(f"place {c+1} == 0")
-            if len(ogr) > 1:
-                ogr = [i for i in ogr if list(i)[c] == "0"]
-        for line in ogr:
-            print(line)
+            csr = [i for i in csr if list(i)[c] == "1"]
 
-    for c in range(len(inputs[0])):
-        print("csr")
-        x = sum([1 for line in inputs if line[c] == "1"])
-        print(x)
-        if x >= ( len(inputs) / 2 ):
-            # There are the same or more 1s than 0s
-            print(f"place {c+1} == 0")
-            if len(csr) > 1:
-                csr = [i for i in csr if list(i)[c] == "0"]
-        else:
-            # There are the same or less 1s than 0s
-            print(f"place {c+1} == 1")
-            if len(csr) > 1:
-                csr = [i for i in csr if list(i)[c] == "1"]
-        for line in csr:
-            print(line)
-
-    ogrd = int(''.join(ogr), 2)
-    csrd = int(''.join(csr), 2)
+    ogrd = int("".join(ogr), 2)
+    csrd = int("".join(csr), 2)
     return ogrd * csrd
-
-
-
-
 
 
 with open("inputs.txt", "r") as f:
