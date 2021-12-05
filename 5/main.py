9755263baad929de0f6af5d4404cdf1997e85a7c
Lucas Crownover <lcrownover127@gmail.com>
2021-12-05 13:42:38 -0800

    first done at least

--- 5/main.py
+++ 5/main.py
@@ -1,40 +1,68 @@
 #!/usr/bin/env python3
 
+def is_horizontal(x1, x2, y1, y2):
+    if (x1 == x2) or (y1 == y2):
+        return True
+    return False
 
-def expand(x1, y1, x2, y2):
+def expand_horizontal(x1, y1, x2, y2):
+    out = []
+    if x1 > x2:
+        x1, x2 = x2, x1
+    if y1 > y2:
+        y1, y2 = y2, y1
+    for x in range(x1, x2+1):
+        for y in range(y1, y2+1):
+            out.append((x, y))
+    return list(set(out))
+
+def expand_diagonal(x1, y1, x2, y2):
     print(f"begin expand: {x1} {y1} {x2} {y2}")
     out = []
-    for x in range(x1, x2):
-        for y in range(y1, y2):
+    dist = abs(x2 - x1)
+    for x in range(dist):
+        if
+
             print(f"appending: ({x}, {y})")
             out.append((x, y))
     print(out)
     print(f"end expand: {x1} {y1} {x2} {y2}")
     return list(set(out))
 
-
 def first(inputs):
+    main = {}
+    for line in inputs:
+        pair1, pair2 = line.split(" -> ")
+        x1, y1 = pair1.split(",")
+        x2, y2 = pair2.split(",")
+        if not is_horizontal(x1, x2, y1, y2):
+            continue
+        for pair in expand_horizontal(int(x1), int(y1), int(x2), int(y2)):
+            if pair in main:
+                main[pair] += 1
+            else:
+                main[pair] = 1
+    return sum([1 for v in main.values() if v >= 2])
+
+
+def second(inputs):
     main = {}
     for line in inputs:
         print(f"processing line: {line}")
         pair1, pair2 = line.split(" -> ")
         x1, y1 = pair1.split(",")
         x2, y2 = pair2.split(",")
-        for pair in expand(int(x1), int(y1), int(x2), int(y2)):
+        if is_horizontal(x1, x2, y1, y2):
+            pairs = expand_horizontal(int(x1), int(y1), int(x2), int(y2))
+        else:
+            pairs = expand_diagonal(int(x1), int(y1), int(x2), int(y2))
+        for pair in pairs:
             print(f"pair processed: {pair}")
             if pair in main:
                 main[pair] += 1
             else:
                 main[pair] = 1
-    c = 0
-    for k, v in main.items():
-        if v >= 2:
-            c += 1
-    return c
-
-
-def second(inputs):
-    pass
+    return sum([1 for v in main.values() if v >= 2])
 
 
 import sys
@@ -43,4 +71,4 @@ with open(sys.argv[1], "r") as f:
 
 print(first(inputs))
 
-print(second(inputs))
+# print(second(inputs))
