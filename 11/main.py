b753e2a2a2905a4e28b54fc6db19f2d862482b00
Lucas Crownover <lcrownover127@gmail.com>
2021-12-12 10:38:58 -0800

    refactored to class, flashes not working because need to retro-flash after iterator...

--- 11/main.py
+++ 11/main.py
@@ -1,92 +1,102 @@
-def display(grid):
-    for row in grid:
-        r = [f"{int(i):02d}" for i in row]
-        print(r)
-    print("\n")
+class Board:
+    def __init__(self, grid):
+        self.count = 0
+        self.steps = 0
+        self.grid = self.parse(grid)
 
-def increase_all_by(number, grid):
-    out = []
-    for row in grid:
-        out.append([int(i) + number for i in row])
-    return out
-
-
-def safe_increase_by(number, x, y, grid):
-    if 0 <= y < len(grid):
-        if 0 <= x < len(grid[0]):
-            grid[y][x] += number
-            print(f"increasing  {x},{y}")
+    @classmethod
+    def parse(cls, grid: list[list[int]]):
+        out = []
+        for row in grid:
+            r = []
+            for elem in row:
+                r.append(int(elem))
+            out.append(r)
+        return out
 
 
-def process_flash(x, y, grid):
-    print(f"processing [{x},{y}] {grid[y][x]}")
-    grid = grid.copy()
-    safe_increase_by(1, x - 1, y - 1, grid)  # top left
-    safe_increase_by(1, x, y - 1, grid)  # top middle
-    safe_increase_by(1, x + 1, y - 1, grid)  # top right
-    safe_increase_by(1, x - 1, y, grid)  # middle left
-    safe_increase_by(1, x, y, grid)  # middle
-    safe_increase_by(1, x + 1, y, grid)  # middle right
-    safe_increase_by(1, x - 1, y + 1, grid)  # bottom left
-    safe_increase_by(1, x, y + 1, grid)  # bottom
-    safe_increase_by(1, x + 1, y + 1, grid)  # bottom right
-    return grid
+    def display(self):
+        for row in self.grid:
+            r = [f"{int(i):02d}" for i in row]
+            print(r)
+        print("\n")
 
+    def increase_all_by(self, number):
+        for y, row in enumerate(self.grid):
+            for x, _ in enumerate(row):
+                self.safe_increase_by(1, x, y)
 
-def flash(grid):
-    grid = grid.copy()
-    for y, row in enumerate(grid):
-        for x, elem in enumerate(row):
-            if int(elem) > 9:
-                grid = process_flash(x, y, grid)
-    return grid
+    def safe_increase_by(self, number: int, x: int, y: int):
+        if 0 <= y < len(self.grid):
+            if 0 <= x < len(self.grid[0]):
+                self.grid[y][x] += number
+                # print(f"increasing  {x},{y}")
 
-def count_and_reset(grid, count):
-    c = 0
-    grid = grid.copy()
-    for y, row in enumerate(grid):
-        for x, elem in enumerate(row):
-            if elem >= 10:
-                c += 1
-                grid[y][x] = 0
-    return grid, c + count
+    def process_flash(self, x: int, y: int):
+        print(f"processing [{x},{y}] {self.grid[y][x]}")
+        self.safe_increase_by(1, x - 1, y - 1)  # top left
+        self.safe_increase_by(1, x, y - 1)  # top middle
+        self.safe_increase_by(1, x + 1, y - 1)  # top right
+        self.safe_increase_by(1, x - 1, y)  # middle left
+        self.safe_increase_by(1, x, y)  # middle
+        self.safe_increase_by(1, x + 1, y)  # middle right
+        self.safe_increase_by(1, x - 1, y + 1)  # bottom left
+        self.safe_increase_by(1, x, y + 1)  # bottom
+        self.safe_increase_by(1, x + 1, y + 1)  # bottom right
+
+    def flash(self):
+        for y, row in enumerate(self.grid):
+            for x, elem in enumerate(row):
+                if int(elem) > 9:
+                    self.process_flash(x, y)
+
+    def count_and_reset(self):
+        for y, row in enumerate(self.grid):
+            for x, elem in enumerate(row):
+                if int(elem) >= 10:
+                    self.count += 1
+                    self.grid[y][x] = 0
+
+    def step(self):
+        banner(self.steps)
+        self.display()
+        self.increase_all_by(1)
+        print("after increase:")
+        self.display()
+        self.flash()
+        print("after flash:")
+        self.display()
+        self.count_and_reset()
+        print("after count and reset:")
+        self.display()
+        self.steps += 1
+        print(f"finished step: {self.steps}")
+        print(f"count: {self.count}")
+        self.display()
+
 
 def banner(steps):
-    print("#"*40)
-    print(" "*10 + f" Starting Step: {steps+1}")
-    print("#"*40)
+    print("#" * 40)
+    print(" " * 10 + f" Starting Step: {steps+1}")
+    print("#" * 40)
 
 
 def first(grid):
-    steps = 0
-    count = 0
-    while steps < 10:
-        banner(steps)
-        display(grid)
-        grid = increase_all_by(1, grid)
-        print("after increase:")
-        display(grid)
-        grid = flash(grid)
-        print("after flash:")
-        display(grid)
-        grid, count = count_and_reset(grid, count)
-        print("after count and reset:")
-        display(grid)
-        steps += 1
-        print(f"finished step: {steps}")
-        print(f"count: {count}")
-        display(grid)
-    return count
+    board = Board(grid)
+    for _ in range(10):
+        board.step()
 
 
 # def second(grid):
 # return grid
 
-# f = "10/test_inputs.txt"
-# f = "10/inputs.txt"
-import sys
+f = "11/test_inputs.txt"
+# f = "11/smaller_test.txt"
+# f = "11/inputs.txt"
 
-with open(sys.argv[1], "r") as f:
+with open(f, "r") as f:
+# import sys
+# with open(sys.argv[1], "r") as f:
     grid = [list(line.strip()) for line in f.readlines() if line.strip()]
 
 print(first(grid))
